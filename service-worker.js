d731ab029913d6fe246c0fd2ac151d69fde40fd9
DpixM <speed_seb@msn.com>
2025-09-27 23:44:34 +0200

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "lishi-cache-v5";
+const CACHE = "lishi-cache-v7";
 const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", e=>{
@@ -16,3 +16,4 @@ self.addEventListener("fetch", e=>{
     }).catch(()=> caches.match("./index.html")))
   );
 });
+
